9edaceaff264064f7a6265af87aad8f659255b5b
sunga <eert2260@naver.com>
2025-01-30 16:04:11 +0900

    컨트롤러에서 ExceptionHandler 사용하여 예외처리

--- src/main/java/todoapp/web/LoginController.java
+++ src/main/java/todoapp/web/LoginController.java
@@ -1,13 +1,14 @@
 package todoapp.web;
 
 import jakarta.validation.Valid;
-import jakarta.validation.ValidationException;
 import jakarta.validation.constraints.Size;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import todoapp.core.user.application.RegisterUser;
@@ -34,13 +35,13 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String loginPage(@Valid LoginCommand loginCommand, BindingResult bindingResult, Model model) {
+    public String loginPage(@Valid LoginCommand loginCommand, BindingResult bindingResult, Model model) { //BindingResult를 컨트롤러에서 받으면 유효성 오류를 발생시키지 않음(컨트롤러 내에서 처리 필요)
         logger.info(loginCommand.username + " : " + loginCommand.password);
 
         //입력값 검증 실패시 login 페이지로 돌려보내고, 오류 메시지 노출
         if (bindingResult.hasErrors()) {
-            model.addAttribute("bindingResult", bindingResult); //입력칸 하단에 어떤 오류인지 나타내기 위해 필요
-            model.addAttribute("message", "입력 값이 없거나 올바르지 않아요."); //login 버튼 하단 오류 메시지
+            model.addAttribute("bindingResult", bindingResult);
+            model.addAttribute("message", "입력 값이 없거나 올바르지 않아요.");
             return "login";
         }
 
@@ -50,12 +51,17 @@ public class LoginController {
         } catch (UserNotFoundException e) {
             //사용자가 없으면 신규 사용자로 등록 후 /todos로 리다이렉트
             registerUser.register(loginCommand.username, loginCommand.password);
-        } catch (UserPasswordNotMatchedException e) {
-            //비밀번호 미일치시 login 페이지로 돌려보내기
-            return "login";
         }
 
         return "redirect:/todos";
+    }
+
+    //비밀번호 미일치시 login 페이지로 돌려보내고, 오류 메시지 노출
+    @ExceptionHandler(UserPasswordNotMatchedException.class)
+    public String handleUserPasswordNotMatchedException(UserPasswordNotMatchedException error, Model model) {
+        model.addAttribute("message", error.getMessage());
+
+        return "login";
     }
 
     record LoginCommand(@Size(min = 4, max = 20) String username, String password) {}
