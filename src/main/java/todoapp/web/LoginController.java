b9c0bb0b67a7be29e33b92256ed88b488cbc3a79
sunga <eert2260@naver.com>
2025-01-30 15:20:03 +0900

    로그인 프로세스 구현 - 입력값 검증 실패시 처리

--- src/main/java/todoapp/web/LoginController.java
+++ src/main/java/todoapp/web/LoginController.java
@@ -10,10 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import todoapp.core.user.application.RegisterUser;
 import todoapp.core.user.application.VerifyUserPassword;
-import todoapp.core.user.domain.User;
 import todoapp.core.user.domain.UserNotFoundException;
 import todoapp.core.user.domain.UserPasswordNotMatchedException;
 
@@ -36,8 +34,15 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String loginPage(LoginCommand loginCommand) {
+    public String loginPage(@Valid LoginCommand loginCommand, BindingResult bindingResult, Model model) {
         logger.info(loginCommand.username + " : " + loginCommand.password);
+
+        //입력값 검증 실패시 login 페이지로 돌려보내고, 오류 메시지 노출
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("bindingResult", bindingResult); //입력칸 하단에 어떤 오류인지 나타내기 위해 필요
+            model.addAttribute("message", "입력 값이 없거나 올바르지 않아요."); //login 버튼 하단 오류 메시지
+            return "login";
+        }
 
         try {
             //비밀번호 일치시 /todos로 리다이렉트
@@ -53,5 +58,5 @@ public class LoginController {
         return "redirect:/todos";
     }
 
-    record LoginCommand(String username, String password) {}
+    record LoginCommand(@Size(min = 4, max = 20) String username, String password) {}
 }
