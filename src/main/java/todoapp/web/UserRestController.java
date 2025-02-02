d3154f1913bdd3ca9d03b60c9428db500c9a430a
sunga <eert2260@naver.com>
2025-02-02 15:42:59 +0900

    프로필 조회 api - @RolesAllowed 사용하여 간단하게 변경

--- src/main/java/todoapp/web/UserRestController.java
+++ src/main/java/todoapp/web/UserRestController.java
@@ -1,23 +1,17 @@
 package todoapp.web;
 
-import jakarta.servlet.http.HttpSession;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import jakarta.annotation.security.RolesAllowed;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import todoapp.security.UserSession;
 import todoapp.web.model.UserProfile;
 
-import java.util.Objects;
-
 @RestController
 public class UserRestController {
 
+    @RolesAllowed(UserSession.ROLE_USER)
     @GetMapping("/api/user/profile")
-    public ResponseEntity<UserProfile> getUserProfile(UserSession userSession) {
-        if (Objects.nonNull(userSession)) {
-            return ResponseEntity.ok(new UserProfile(userSession.getUser()));
-        }
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    public UserProfile getUserProfile(UserSession userSession) {
+        return new UserProfile(userSession.getUser());
     }
 }
